c382ce34d2e0acdc15837eba521238d7fd097634
Shammi-Akther <shammisumi137@gmail.com.>
2025-10-17 12:44:39 +0600

    Fix some small issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div >
      <Navbar />
+     <Hero />
       <h1>
         hello from home
       </h1>
